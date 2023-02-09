28d7ccc04b24478f8b13b8707c0933f26bd5881d
Kuzmin Kirill <kirillkuzminr@gmail.com>
2023-02-09 03:04:36 +0300

    Create objects matrix and vectors

--- src/com/numericalCore/task0/Objects/Matrix.java
+++ src/com/numericalCore/task0/Objects/Matrix.java
@@ -1,4 +1,40 @@
 package com.numericalCore.task0.Objects;
 
 public class Matrix {
+
+    private final int rank;
+
+    private final double [][] matrix;
+
+
+    public Matrix(int rank) {
+        this.rank = rank;
+        matrix = new double[rank][rank];
+    }
+
+
+    public Matrix(int rank, double [][] matrix) {
+        this.rank = rank;
+        this.matrix = matrix;
+    }
+
+
+    public Vectors multByVector(Vectors vector) {
+
+        Vectors result = new Vectors(vector.getRank());
+
+        double sum = 0;
+
+        for (int i = 0; i < rank; i++) {
+            for (int j = 0; j < rank; j++) {
+                sum += matrix[i][j] * vector.get(j);
+            }
+            result.set(i, sum);
+
+            sum = 0;
+        }
+
+        return result;
+    }
+
 }
