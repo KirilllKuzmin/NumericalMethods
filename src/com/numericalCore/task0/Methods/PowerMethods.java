49cc13afba7aaf1a9570bcaa60b4f9083b8e2af4
Kuzmin Kirill <kirillkuzminr@gmail.com>
2023-02-09 03:09:41 +0300

    Added program launch

--- src/com/numericalCore/task0/Methods/PowerMethods.java
+++ src/com/numericalCore/task0/Methods/PowerMethods.java
@@ -1,4 +1,13 @@
 package com.numericalCore.task0.Methods;
 
+import com.numericalCore.task0.Objects.Matrix;
+import com.numericalCore.task0.Objects.Vectors;
+
 public class PowerMethods {
+
+    public static double start(Matrix matrix, Vectors approximateVector, double eps) {
+
+        return 0;
+    }
+
 }
